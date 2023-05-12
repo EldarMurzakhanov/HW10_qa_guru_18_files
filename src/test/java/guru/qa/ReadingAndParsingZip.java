5aa82e52def0430c2fc59427094c9387caa66122
EldarMurzakhanov <eldarmurzahanov@gmail.com>
2023-05-12 03:06:02 +0300

    day 2

---
+++ src/test/java/guru/qa/ReadingAndParsingZip.java
@@ -0,0 +1,30 @@
+package guru.qa;
+
+import com.codeborne.pdftest.PDF;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ReadingAndParsingZip {
+
+    private ClassLoader cl = ReadingAndParsingZip.class.getClassLoader();
+
+    @Test
+    @DisplayName("Чтение PDF из архива zip")
+    void testPDF() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("HW10.zip");
+             ZipInputStream zs = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zs.getNextEntry()) != null) {
+                if (entry.getName().equals("ДЗ_DevTools_Воркшоп_1.pdf")) {
+                    PDF pdf = new PDF(zs);
+                    Assertions.assertTrue(entry.getName().contains("ДЗ_DevTools_Воркшоп_1.pdf"));
+                }
+            }
+        }
+    }
+}
