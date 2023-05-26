6d997b5867767af2ecfc340736d5c8290e4f2ed8
EldarMurzakhanov <eldarmurzahanov@gmail.com>
2023-05-26 11:18:15 +0300

    commit Json parsing test

---
+++ src/test/java/guru/qa/JsonObject.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import java.util.List;
+public class JsonObject {
+    public String title;
+    public String genre;
+    public Integer release;
+    public Boolean retro;
+    public List<String> developers;
+    public List<String> designers;
+    public List<String> fighters;
+}
--- src/test/java/guru/qa/JsonParsingTest.java
+++ src/test/java/guru/qa/JsonParsingTest.java
@@ -1,4 +1,29 @@
 package guru.qa;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonParsingTest {
-}
+
+    private ClassLoader cl = ReadingAndParsingZip.class.getClassLoader();
+
+    @Test
+    @DisplayName("Парсинг JSON")
+    void testJson() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        try (InputStream is = cl.getResourceAsStream("HomeWork.json");
+             InputStreamReader isr = new InputStreamReader(is)) {
+            JsonObject jsonObject = mapper.readValue(isr, JsonObject.class);
+
+                    Assertions.assertTrue(jsonObject.retro);
+                    Assertions.assertEquals(1992, jsonObject.release);
+                }
+            }
+        }
